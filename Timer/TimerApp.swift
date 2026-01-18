7de7467e22fb59407c5f75e48b6b260e1190392b
flucium <flucium@flucium.net>
2026-01-19 00:20:28 +0900

    update ./Timer/TimerApp.swift

--- Timer/TimerApp.swift
+++ Timer/TimerApp.swift
@@ -5,15 +5,6 @@ struct TimerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(
-                    minWidth: 500,
-                    idealWidth: 500,
-                    maxWidth: 500,
-                    minHeight: 600,
-                    idealHeight: 600,
-                    maxHeight: 600,
-                    alignment: .center
-                )
-        }.windowResizability(.contentSize)
+        }
     }
 }
