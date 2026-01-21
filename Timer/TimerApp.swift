4cb5a8f5e12c726bff9b11a0b381d8374df351c0
flucium <flucium@flucium.net>
2026-01-21 10:56:15 +0900

    update ./Timer/TimerApp.swift

--- Timer/TimerApp.swift
+++ Timer/TimerApp.swift
@@ -1,4 +1,6 @@
+import Foundation
 import SwiftUI
+import SwiftData
 
 @main
 struct TimerApp: App {
@@ -6,5 +8,25 @@ struct TimerApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: HistoryEntry.self)
+#if os(macOS)
+        .commands {
+            CommandGroup(after: .textEditing) {
+                Button("Delete") {
+                    NotificationCenter.default
+                        .post(name: .deleteHistorySelection, object: nil)
+                }
+                .keyboardShortcut(.delete)
+            }
+        }
+#endif // os(macOS)
     }
+}
+
+
+// extension (Delete)
+extension Notification.Name {
+    static let deleteHistorySelection = Notification.Name(
+        "deleteHistorySelection"
+    )
 }
